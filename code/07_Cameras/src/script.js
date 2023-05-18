628e21a0a60711d7956d2d87dbc318d7a178a59b
Michelangelo Lv <mgaligigi@163.com>
2023-05-18 17:14:09 +0800

    feat:07_Cameras

---
+++ code/07_Cameras/src/script.js
@@ -0,0 +1,88 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height,1,1000)
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100)
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
+// camera.lookAt(mesh.position)
+scene.add(camera)
+
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+// controls.target.y = 2
+controls.enableDamping = true
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener("mousemove",(event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = -(event.clientY / sizes.height - 0.5)
+
+    console.log(cursor.x,cursor.y);
+})
+
+// Animate
+const clock = new THREE.Clock()
+
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    // mesh.rotation.y = elapsedTime;
+    // camera.position.x = cursor.x * 5
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
+    // camera.position.y = cursor.y * 3
+    // camera.lookAt(mesh.position)
+    controls.update()
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
