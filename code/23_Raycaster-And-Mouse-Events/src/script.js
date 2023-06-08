fbe2444b162c2a513af770238f55c1de75dfca45
Michelangelo Lv <mgaligigi@163.com>
2023-06-08 17:35:11 +0800

    feat(23_Raycaster-And-Mouse-Events):Hovering

--- code/23_Raycaster-And-Mouse-Events/src/script.js
+++ code/23_Raycaster-And-Mouse-Events/src/script.js
@@ -53,6 +53,18 @@ const raycaster = new THREE.Raycaster();
 // console.log(intersects);
 
 /**
+ * Mouse
+ */
+const mouse = new THREE.Vector2();
+
+window.addEventListener("mousemove", (event) => {
+  mouse.x = (event.clientX / sizes.width) * 2 - 1;
+  mouse.y = -(event.clientY / sizes.height) * 2 + 1;
+
+  console.log(mouse);
+});
+
+/**
  * Sizes
  */
 const sizes = {
@@ -109,27 +121,44 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Cast a ray
-  const rayOrigin = new THREE.Vector3(-3, 0, 0);
-  const rayDirection = new THREE.Vector3(1, 0, 0);
-  rayDirection.normalize();
+  // const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  // const rayDirection = new THREE.Vector3(1, 0, 0);
+  // rayDirection.normalize();
 
-  raycaster.set(rayOrigin, rayDirection);
+  // raycaster.set(rayOrigin, rayDirection);
 
-  const objectsToTest = [object1, object2, object3];
-  const intersects = raycaster.intersectObjects(objectsToTest);
-  console.log(intersects);
+  // const objectsToTest = [object1, object2, object3];
+  // const intersects = raycaster.intersectObjects(objectsToTest);
+  // console.log(intersects);
+
 
   // Animate objects
   object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
   object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
   object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
 
-  for (const object of objectsToTest) {
-    object.material.color.set("#ff0000");
-  }
+  // for (const object of objectsToTest) {
+  //   object.material.color.set("#ff0000");
+  // }
+
+  // for (const intersect of intersects) {
+  //   intersect.object.material.color.set("#0000ff");
+  // }
+
+  // Mouse Raycaster
+  raycaster.setFromCamera(mouse, camera);
+
+  const objectsToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(objectsToTest);
 
   for (const intersect of intersects) {
     intersect.object.material.color.set("#0000ff");
+  }
+
+  for (const object of objectsToTest) {
+    if (!intersects.find((intersect) => intersect.object === object)) {
+      object.material.color.set("#ff0000");
+    }
   }
 
   // Update controls
