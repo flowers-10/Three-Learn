e4bd89f3ba3a11e98f3dd41acbe0306aefadd67f
flowers-10 <mgaligigi@163.com>
2023-07-01 23:55:37 +0800

    feat(27_Code...): Useful classes

--- code/27_Code-Structuring-For-Bigger-Projects/src/Experience/Experience.js
+++ code/27_Code-Structuring-For-Bigger-Projects/src/Experience/Experience.js
@@ -1,8 +1,30 @@
+import Sizes from "./Utils/Sizes";
+import Time from "./Utils/Time.js";
+
 export default class Experience {
   constructor(canvas) {
     // Global access
     window.experience = this;
     // Options
     this.canvas = canvas;
+
+    // Setup
+    this.sizes = new Sizes();
+    this.time = new Time();
+    // console.log(this.sizes.width)
+    // console.log(this.sizes.height)
+    // console.log(this.sizes.pixelRatio)
+
+    // Resize event
+    this.sizes.on("resize", () => {
+      this.resize();
+    });
+
+    // Time tick event
+    this.time.on("tick", () => {
+      this.update();
+    });
   }
+  resize() {}
+  update() {}
 }
