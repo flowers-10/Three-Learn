0da650daaee54614870a48be074047c4d7948d28
flowers-10 <mgaligigi@163.com>
2023-07-02 22:11:58 +0800

    feat(27_Code...): Environment map

--- code/27_Code-Structuring-For-Bigger-Projects/src/Experience/World/Environment.js
+++ code/27_Code-Structuring-For-Bigger-Projects/src/Experience/World/Environment.js
@@ -1,25 +1,47 @@
-import * as THREE from 'three'
-import Experience from '../Experience.js'
+import * as THREE from "three";
+import Experience from "../Experience.js";
 
-export default class Environment
-{
-    constructor()
-    {
-        this.experience = new Experience()
-        this.scene = this.experience.scene
+export default class Environment {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
 
-         // Setup
-         this.setSunLight()
-    }
+    // Setup
+    this.setSunLight();
+    this.setEnvironmentMap();
+  }
 
-    setSunLight()
-    {
-        this.sunLight = new THREE.DirectionalLight('#ffffff', 4)
-        this.sunLight.castShadow = true
-        this.sunLight.shadow.camera.far = 15
-        this.sunLight.shadow.mapSize.set(1024, 1024)
-        this.sunLight.shadow.normalBias = 0.05
-        this.sunLight.position.set(3, 3, - 2.25)
-        this.scene.add(this.sunLight)
-    }
+  setSunLight() {
+    this.sunLight = new THREE.DirectionalLight("#ffffff", 4);
+    this.sunLight.castShadow = true;
+    this.sunLight.shadow.camera.far = 15;
+    this.sunLight.shadow.mapSize.set(1024, 1024);
+    this.sunLight.shadow.normalBias = 0.05;
+    this.sunLight.position.set(3, 3, -2.25);
+    this.scene.add(this.sunLight);
+  }
+
+  setEnvironmentMap() {
+    this.environmentMap = {};
+    this.environmentMap.intensity = 0.4;
+    this.environmentMap.texture = this.resources.items.environmentMapTexture;
+    // this.environmentMap.texture.encoding = THREE.sRGBEncoding;
+
+    this.scene.environment = this.environmentMap.texture;
+
+    this.environmentMap.updateMaterials = () =>
+        {
+            this.scene.traverse((child) =>
+            {
+                if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
+                {
+                    child.material.envMap = this.environmentMap.texture
+                    child.material.envMapIntensity = this.environmentMap.intensity
+                    child.material.needsUpdate = true
+                }
+            })
+        }
+        this.environmentMap.updateMaterials()
+  }
 }
