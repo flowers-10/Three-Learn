f044f76cd5316722d7204bd08024589eafb29185
flowers-10 <mgaligigi@163.com>
2023-07-03 22:47:14 +0800

    feat(27_Code...): Destroying

--- code/27_Code-Structuring-For-Bigger-Projects/src/Experience/Experience.js
+++ code/27_Code-Structuring-For-Bigger-Projects/src/Experience/Experience.js
@@ -2,12 +2,11 @@ import * as THREE from "three";
 import Sizes from "./Utils/Sizes";
 import Time from "./Utils/Time.js";
 import Camera from "./Camera.js";
-import World from './World/World.js'
-import Renderer from './Renderer'
-import Resources from './Utils/Resources.js'
-import sources from './sources.js'
-import Debug from './Utils/Debug.js'
-
+import World from "./World/World.js";
+import Renderer from "./Renderer";
+import Resources from "./Utils/Resources.js";
+import sources from "./sources.js";
+import Debug from "./Utils/Debug.js";
 
 let instance = null;
 
@@ -25,14 +24,14 @@ export default class Experience {
     this.canvas = canvas;
 
     // Setup
-    this.debug = new Debug()
+    this.debug = new Debug();
     this.sizes = new Sizes();
     this.time = new Time();
     this.scene = new THREE.Scene();
-    this.resources = new Resources(sources)
+    this.resources = new Resources(sources);
     this.camera = new Camera();
-    this.renderer = new Renderer()
-    this.world = new World()
+    this.renderer = new Renderer();
+    this.world = new World();
 
     // console.log(this.sizes.width)
     // console.log(this.sizes.height)
@@ -50,12 +49,39 @@ export default class Experience {
   }
   resize() {
     this.camera.resize();
-    this.renderer.resize()
+    this.renderer.resize();
   }
-  update()
-  {
-      this.camera.update()
-      this.world.update()
-      this.renderer.update()
+  update() {
+    this.camera.update();
+    this.world.update();
+    this.renderer.update();
+  }
+
+  destroy() {
+    this.sizes.off("resize");
+    this.time.off("tick");
+
+    // Traverse the whole scene
+    this.scene.traverse((child) => {
+      // Test if it's a mesh
+      if (child instanceof THREE.Mesh) {
+        child.geometry.dispose();
+
+        // Loop through the material properties
+        for (const key in child.material) {
+          const value = child.material[key];
+
+          // Test if there is a dispose function
+          if (value && typeof value.dispose === "function") {
+            value.dispose();
+          }
+        }
+      }
+    });
+
+    this.camera.controls.dispose();
+    this.renderer.instance.dispose();
+    if (this.debug.active) this.debug.ui.destroy();
+    document.querySelector('.webgl').remove()
   }
 }
