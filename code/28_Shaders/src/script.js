cd35033f5f778edbbb3241b5ff6e4617d29e0210
Michela <mgaligigi@163.com>
2023-08-15 23:12:20 +0800

    feat(28_Shaders):shaders files

--- code/28_Shaders/src/script.js
+++ code/28_Shaders/src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import testVertexShader from './shaders/test/vertex.glsl'
+import testFragmentShader from './shaders/test/fragment.glsl'
 
 /**
  * Base
@@ -27,23 +29,9 @@ const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
 // Material
 const material = new THREE.RawShaderMaterial({
-    vertexShader:`
-    uniform mat4 projectionMatrix;
-    uniform mat4 viewMatrix;
-    uniform mat4 modelMatrix;
-
-    attribute vec3 position;
-    void main(){
-        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-    }
-    `,
-    fragmentShader:`
-    precision mediump float;
-
-    void main() {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    }
-    `
+    vertexShader:testVertexShader,
+    fragmentShader:testFragmentShader,
+    wireframe:true
 })
 
 // Mesh
