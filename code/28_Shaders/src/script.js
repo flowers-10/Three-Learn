e2e6bc2695558c9e414e827c699812a2d38711ce
Michela <mgaligigi@163.com>
2023-08-15 22:52:00 +0800

    feat(28_Shaders):first shaders with RawSHaderMaterial

--- code/28_Shaders/src/script.js
+++ code/28_Shaders/src/script.js
@@ -26,7 +26,25 @@ const textureLoader = new THREE.TextureLoader()
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
 // Material
-const material = new THREE.MeshBasicMaterial()
+const material = new THREE.RawShaderMaterial({
+    vertexShader:`
+    uniform mat4 projectionMatrix;
+    uniform mat4 viewMatrix;
+    uniform mat4 modelMatrix;
+
+    attribute vec3 position;
+    void main(){
+        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    }
+    `,
+    fragmentShader:`
+    precision mediump float;
+
+    void main() {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+    `
+})
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
