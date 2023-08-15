defce31dfd98b317b389636a359fac184edd497a
Michela <mgaligigi@163.com>
2023-08-15 23:36:05 +0800

    feat(28_Shaders):Matrkces uniforms

--- code/28_Shaders/src/shaders/test/vertex.glsl
+++ code/28_Shaders/src/shaders/test/vertex.glsl
@@ -1,8 +1,13 @@
 uniform mat4 projectionMatrix;
-    uniform mat4 viewMatrix;
-    uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
 
-    attribute vec3 position;
-    void main(){
-        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-    }
+attribute vec3 position;
+void main(){
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+}
