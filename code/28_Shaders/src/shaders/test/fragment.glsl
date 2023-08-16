5d6ed244f1766eed00967fc41e1c67b174b086d3
flowers-10 <mgaligigi@163.com>
2023-08-16 22:29:20 +0800

    feat(28_Shaders):Test values

--- code/28_Shaders/src/shaders/test/fragment.glsl
+++ code/28_Shaders/src/shaders/test/fragment.glsl
@@ -8,7 +8,5 @@ varying float vElevation;
 
 void main()
 {   
-    vec4 textureColor = texture2D(uTexture, vUv);
-    textureColor.rgb *= vElevation * 2.0 + 0.5;
-    gl_FragColor = textureColor;
+    gl_FragColor = vec4(vUv, 1.0, 1.0);
 }
