5ea3ac73921618821771c2779f02ed93df256f6e
flowers-10 <mgaligigi@163.com>
2023-08-16 22:22:35 +0800

    feat(28_Shaders):Color variations

--- code/28_Shaders/src/shaders/test/fragment.glsl
+++ code/28_Shaders/src/shaders/test/fragment.glsl
@@ -4,10 +4,11 @@ uniform vec3 uColor;
 uniform sampler2D uTexture;
 
 varying vec2 vUv;
-
+varying float vElevation;
 
 void main()
 {   
     vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb *= vElevation * 2.0 + 0.5;
     gl_FragColor = textureColor;
 }
