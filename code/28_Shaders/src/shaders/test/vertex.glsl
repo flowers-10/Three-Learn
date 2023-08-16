585ad5303d9320f29da55a7fc5a771d568820fd4
flowers-10 <mgaligigi@163.com>
2023-08-16 22:11:10 +0800

    feat(28_Shaders):Uniform

--- code/28_Shaders/src/shaders/test/vertex.glsl
+++ code/28_Shaders/src/shaders/test/vertex.glsl
@@ -2,12 +2,19 @@ uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 
+uniform vec2 uFrequency;
+uniform float uTime;
+// attribute float aRandom;
+// varying float vRandom;
 attribute vec3 position;
 void main(){
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+    // modelPosition.z += aRandom * 0.1;
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
-
     gl_Position = projectedPosition;
+    // vRandom = aRandom;
+
 }
