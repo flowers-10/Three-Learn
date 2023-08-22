7d375ea42f447e7deb6d204a08e1ce1eeaa2aaca
Michela <mgaligigi@163.com>
2023-08-22 22:30:07 +0800

    feat(29_Shader_Patterns):Pattern 10

--- code/29_Shader_Patterns/src/shaders/test/fragment.glsl
+++ code/29_Shader_Patterns/src/shaders/test/fragment.glsl
@@ -1,7 +1,7 @@
 varying vec2 vUv;
 void main()
 {   
-    float strength = mod(vUv.y * 10.0, 1.0);
+    float strength = mod(vUv.x * 10.0, 1.0);
     strength = step(0.8, strength);
     gl_FragColor = vec4(vec3(strength), 1.0);
 
