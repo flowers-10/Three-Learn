3d1b21e4154b5cca19d747bedd711c40ea65a296
flowers-10 <mgaligigi@163.com>
2023-08-23 23:26:42 +0800

    feat(29_Shader_Patterns):Pattern 13

--- code/29_Shader_Patterns/src/shaders/test/fragment.glsl
+++ code/29_Shader_Patterns/src/shaders/test/fragment.glsl
@@ -1,7 +1,7 @@
 varying vec2 vUv;
 void main()
 {   
-    float strength = step(0.8,mod(vUv.x *10.0, 1.0));
+    float strength = step(0.4,mod(vUv.x *10.0, 1.0));
     strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
     gl_FragColor = vec4(vec3(strength), 10.0);
 
