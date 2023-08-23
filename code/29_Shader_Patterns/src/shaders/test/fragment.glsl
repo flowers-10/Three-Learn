9540d8fb6eb1eedb35661944866cb1caef18ff9a
flowers-10 <mgaligigi@163.com>
2023-08-23 23:34:29 +0800

    feat(29_Shader_Patterns):Pattern 15

--- code/29_Shader_Patterns/src/shaders/test/fragment.glsl
+++ code/29_Shader_Patterns/src/shaders/test/fragment.glsl
@@ -1,7 +1,7 @@
 varying vec2 vUv;
 void main()
 {   
-    float barX = step(0.4, mod(vUv.x * 10.0 - 0.9, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+    float barX = step(0.4, mod(vUv.x * 10.0 - 0.2, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
     float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4, mod(vUv.y * 10.0 - 0.2, 1.0));
     float strength = barX + barY;
 
