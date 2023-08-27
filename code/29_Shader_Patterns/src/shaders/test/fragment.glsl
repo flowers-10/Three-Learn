3dc8136c336a0ba0766eae353963eb57de76633b
Michela <mgaligigi@163.com>
2023-08-27 14:15:40 +0800

    feat(29_Shader_Patterns):Pattern 26

--- code/29_Shader_Patterns/src/shaders/test/fragment.glsl
+++ code/29_Shader_Patterns/src/shaders/test/fragment.glsl
@@ -7,8 +7,7 @@ float random(vec2 st)
 
 void main()
 {   
-   vec2 gridUv = vec2(floor(vUv.x * 10.0) / 10.0, floor((vUv.y + vUv.x * 0.5) * 10.0) / 10.0);
-float strength = random(gridUv);git a
+    float strength = length(vUv);
 
     gl_FragColor = vec4(vec3(strength), 10.0);
 
