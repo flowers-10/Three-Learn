35ba4859b3702c707ad0c516ff3ba86083d4075a
Michela <mgaligigi@163.com>
2023-08-31 17:46:59 +0800

    feat(29_Shader_Patterns):Pattern 32

--- code/29_Shader_Patterns/src/shaders/test/fragment.glsl
+++ code/29_Shader_Patterns/src/shaders/test/fragment.glsl
@@ -1,9 +1,20 @@
 varying vec2 vUv;
 
+#define PI 3.1415926535897932384626433832795
+
+vec2 rotate(vec2 uv, float rotation, vec2 mid)
+{
+    return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
+}
+
 void main()
 {   
-    float strength = 0.15 / ( distance(vec2(vUv.x,(vUv.y - 0.5) * 5.0 + 0.5),vec2(0.5)) );
-    strength *= 0.15 / ( distance(vec2((vUv.x - 0.5) * 5.0 + 0.5,vUv.y), vec2(0.5)) );
+    vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5));
+    float strength = 0.15 / (distance(vec2(rotatedUv.x, (rotatedUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    strength *= 0.15 / (distance(vec2(rotatedUv.y, (rotatedUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
