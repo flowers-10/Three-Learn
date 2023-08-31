333fe7b8650af93060c873f9de1d14d8be718060
Michela <mgaligigi@163.com>
2023-08-31 17:03:54 +0800

    feat(29_Shader_Patterns):Pattern 31

--- code/29_Shader_Patterns/src/shaders/test/fragment.glsl
+++ code/29_Shader_Patterns/src/shaders/test/fragment.glsl
@@ -3,7 +3,7 @@ varying vec2 vUv;
 void main()
 {   
     float strength = 0.15 / ( distance(vec2(vUv.x,(vUv.y - 0.5) * 5.0 + 0.5),vec2(0.5)) );
+    strength *= 0.15 / ( distance(vec2((vUv.x - 0.5) * 5.0 + 0.5,vUv.y), vec2(0.5)) );
 
     gl_FragColor = vec4(vec3(strength), 1.0);
-
 }
