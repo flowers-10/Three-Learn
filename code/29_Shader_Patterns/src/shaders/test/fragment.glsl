a49a21e6b95d68c35030a213c4953a50c0c02448
Michela <mgaligigi@163.com>
2023-09-02 23:32:30 +0800

    feat(29_Shader_Patterns):Pattern 39

--- code/29_Shader_Patterns/src/shaders/test/fragment.glsl
+++ code/29_Shader_Patterns/src/shaders/test/fragment.glsl
@@ -5,10 +5,8 @@ varying vec2 vUv;
 void main()
 {   
     vec2 wavedUv = vec2(
-        vUv.x + sin(vUv.y * 30.0) * 0.1,
-        vUv.y + sin(vUv.x * 30.0) * 0.1
-
-        
+        vUv.x + sin(vUv.y * 100.0) * 0.1,
+        vUv.y + sin(vUv.x * 100.0) * 0.1     
     );
     float strength = 1.0 - step(0.02, abs(distance(wavedUv, vec2(0.5)) - 0.25));
 
