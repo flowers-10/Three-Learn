057c13d23b85f26798ab4173f2ceff4885e94a46
Michela <mgaligigi@163.com>
2023-09-02 23:41:41 +0800

    feat(29_Shader_Patterns):Pattern 42

--- code/29_Shader_Patterns/src/shaders/test/fragment.glsl
+++ code/29_Shader_Patterns/src/shaders/test/fragment.glsl
@@ -1,10 +1,10 @@
 varying vec2 vUv;
-
+#define PI 3.1415926535897932384626433832795
 
 
 void main()
 {   
-    float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    float angle = atan(vUv.x - 0.5, vUv.y - 0.5) / (PI * 2.0) + 0.5;
     float strength = angle;
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
