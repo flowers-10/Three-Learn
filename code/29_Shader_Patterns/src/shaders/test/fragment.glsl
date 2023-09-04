eaeb86639d03bf280120adb00084d7f544c81edf
Michela <mgaligigi@163.com>
2023-09-04 17:58:11 +0800

    feat(29_Shader_Patterns):Pattern 45

--- code/29_Shader_Patterns/src/shaders/test/fragment.glsl
+++ code/29_Shader_Patterns/src/shaders/test/fragment.glsl
@@ -5,6 +5,7 @@ varying vec2 vUv;
 void main()
 {   
     float angle = atan(vUv.x - 0.5, vUv.y - 0.5) / (PI * 2.0) + 0.5;
-    float strength = sin(angle * 100.0);
+    float radius = 0.25 + sin(angle * 100.0) * 0.02;
+    float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
