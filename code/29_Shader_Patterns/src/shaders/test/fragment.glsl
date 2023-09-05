3158e53fb99e79e6ec75fbee6a5f1636da19a058
Michela <mgaligigi@163.com>
2023-09-05 17:55:57 +0800

    feat(29_Shader_Patterns):Pattern 47

--- code/29_Shader_Patterns/src/shaders/test/fragment.glsl
+++ code/29_Shader_Patterns/src/shaders/test/fragment.glsl
@@ -50,6 +50,6 @@ float cnoise(vec2 P)
 
 void main()
 {   
-    float strength = cnoise(vUv * 15.0);
+    float strength = step(0.0,cnoise(vUv * 10.0));
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
