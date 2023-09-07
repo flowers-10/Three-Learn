4af8a37333bbd698eae38fdd8fe1cfdd96337ada
flowers-10 <mgaligigi@163.com>
2023-09-07 22:28:12 +0800

    feat(30_Raging_Sea):Basie glsl

--- code/30_Raging_Sea/src/script.js
+++ code/30_Raging_Sea/src/script.js
@@ -2,6 +2,9 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 
+import waterVertexShader from './shaders/water/vertex.glsl'
+import waterFragmentShader from './shaders/water/fragment.glsl'
+
 THREE.ColorManagement.enabled = false
 
 /**
@@ -23,7 +26,10 @@ const scene = new THREE.Scene()
 const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
 
 // Material
-const waterMaterial = new THREE.MeshBasicMaterial()
+const waterMaterial = new THREE.ShaderMaterial({
+    vertexShader: waterVertexShader,
+    fragmentShader: waterFragmentShader
+})
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
