ab66cbacc13f4a10825b094425e605a8091998c3
Michela <mgaligigi@163.com>
2023-09-14 20:08:08 +0800

    feat(31_Animated_Galaxy):Diffuse point pattern

--- code/31_Animated_Galaxy/src/shaders/galaxy/fragment.glsl
+++ code/31_Animated_Galaxy/src/shaders/galaxy/fragment.glsl
@@ -1,8 +1,14 @@
 void main()
 {   
     // Disc
+    // float strength = distance(gl_PointCoord, vec2(0.5));
+    // strength = step(0.5, strength);
+    // strength = 1.0 - strength;
+    
+    // Diffuse point
     float strength = distance(gl_PointCoord, vec2(0.5));
-    strength = step(0.5, strength);
+    strength *= 2.0;
     strength = 1.0 - strength;
+
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
