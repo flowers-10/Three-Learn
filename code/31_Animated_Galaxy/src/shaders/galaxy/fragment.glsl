73092eecf3d923a6790076a4e88fcc780c32507d
Michela <mgaligigi@163.com>
2023-09-14 19:03:01 +0800

    feat(31_Animated_Galaxy):Disc pattern

--- code/31_Animated_Galaxy/src/shaders/galaxy/fragment.glsl
+++ code/31_Animated_Galaxy/src/shaders/galaxy/fragment.glsl
@@ -1,4 +1,8 @@
 void main()
-{
-    gl_FragColor = vec4(gl_PointCoord, 1.0, 1.0);
+{   
+    // Disc
+    float strength = distance(gl_PointCoord, vec2(0.5));
+    strength = step(0.5, strength);
+    strength = 1.0 - strength;
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
