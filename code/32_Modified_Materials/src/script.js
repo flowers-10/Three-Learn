17de66ce92417fd01ce696934ea439f53a204f43
Michela <mgaligigi@163.com>
2023-09-16 20:17:47 +0800

    feat(32_Modified_Materials):Hooking the material

--- code/32_Modified_Materials/src/script.js
+++ code/32_Modified_Materials/src/script.js
@@ -69,6 +69,11 @@ const material = new THREE.MeshStandardMaterial( {
     normalMap: normalTexture
 })
 
+material.onBeforeCompile = (shader) =>
+{
+    console.log(shader)
+}
+
 /**
  * Models
  */
