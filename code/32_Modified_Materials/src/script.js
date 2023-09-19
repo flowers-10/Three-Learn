abc0344768e988cb09c95cf8b5609d6629d6c4a8
Michela <mgaligigi@163.com>
2023-09-19 09:20:54 +0800

    feat(32_Modified_Materials):Animating

--- code/32_Modified_Materials/src/script.js
+++ code/32_Modified_Materials/src/script.js
@@ -1,196 +1,204 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import * as dat from 'lil-gui'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import * as dat from "lil-gui";
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Loaders
  */
-const textureLoader = new THREE.TextureLoader()
-const gltfLoader = new GLTFLoader()
-const cubeTextureLoader = new THREE.CubeTextureLoader()
+const textureLoader = new THREE.TextureLoader();
+const gltfLoader = new GLTFLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
 
 /**
  * Update all materials
  */
-const updateAllMaterials = () =>
-{
-    scene.traverse((child) =>
-    {
-        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
-        {
-            child.material.envMapIntensity = 1
-            child.material.needsUpdate = true
-            child.castShadow = true
-            child.receiveShadow = true
-        }
-    })
-}
+const updateAllMaterials = () => {
+  scene.traverse((child) => {
+    if (
+      child instanceof THREE.Mesh &&
+      child.material instanceof THREE.MeshStandardMaterial
+    ) {
+      child.material.envMapIntensity = 1;
+      child.material.needsUpdate = true;
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+};
 
 /**
  * Environment map
  */
 const environmentMap = cubeTextureLoader.load([
-    '/textures/environmentMaps/0/px.jpg',
-    '/textures/environmentMaps/0/nx.jpg',
-    '/textures/environmentMaps/0/py.jpg',
-    '/textures/environmentMaps/0/ny.jpg',
-    '/textures/environmentMaps/0/pz.jpg',
-    '/textures/environmentMaps/0/nz.jpg'
-])
+  "/textures/environmentMaps/0/px.jpg",
+  "/textures/environmentMaps/0/nx.jpg",
+  "/textures/environmentMaps/0/py.jpg",
+  "/textures/environmentMaps/0/ny.jpg",
+  "/textures/environmentMaps/0/pz.jpg",
+  "/textures/environmentMaps/0/nz.jpg",
+]);
 
-scene.background = environmentMap
-scene.environment = environmentMap
+scene.background = environmentMap;
+scene.environment = environmentMap;
 
 /**
  * Material
  */
 
 // Textures
-const mapTexture = textureLoader.load('/models/LeePerrySmith/color.jpg')
-mapTexture.colorSpace = THREE.SRGBColorSpace
-const normalTexture = textureLoader.load('/models/LeePerrySmith/normal.jpg')
+const mapTexture = textureLoader.load("/models/LeePerrySmith/color.jpg");
+mapTexture.colorSpace = THREE.SRGBColorSpace;
+const normalTexture = textureLoader.load("/models/LeePerrySmith/normal.jpg");
 
 // Material
-const material = new THREE.MeshStandardMaterial( {
-    map: mapTexture,
-    normalMap: normalTexture
-})
+const material = new THREE.MeshStandardMaterial({
+  map: mapTexture,
+  normalMap: normalTexture,
+});
 
-material.onBeforeCompile = (shader) =>
-{   
-console.log(shader.vertexShader);
+const customUniforms = {
+    uTime: { value: 0 }
+}
 
-    shader.vertexShader = shader.vertexShader.replace(
-        '#include <common>',
-        `
+material.onBeforeCompile = (shader) => {
+  console.log(shader.vertexShader);
+  shader.uniforms.uTime = customUniforms.uTime
+  shader.vertexShader = shader.vertexShader.replace(
+    "#include <common>",
+    `
             #include <common>
+
+            uniform float uTime;
 
             mat2 get2dRotateMatrix(float _angle)
             {
                 return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
             }
         `
-    )
-    shader.vertexShader = shader.vertexShader.replace('#include <begin_vertex>',
+  );
+  shader.vertexShader = shader.vertexShader.replace(
+    "#include <begin_vertex>",
     `
         #include <begin_vertex>
-        float angle = position.y * 0.9;
+        float angle = (position.y + uTime) * 0.9;
 
         mat2 rotateMatrix = get2dRotateMatrix(angle);
         transformed.xz = rotateMatrix * transformed.xz;
-    `)
-
-}
+    `
+  );
+};
 
 /**
  * Models
  */
-gltfLoader.load(
-    '/models/LeePerrySmith/LeePerrySmith.glb',
-    (gltf) =>
-    {
-        // Model
-        const mesh = gltf.scene.children[0]
-        mesh.rotation.y = Math.PI * 0.5
-        mesh.material = material
-        scene.add(mesh)
+gltfLoader.load("/models/LeePerrySmith/LeePerrySmith.glb", (gltf) => {
+  // Model
+  const mesh = gltf.scene.children[0];
+  mesh.rotation.y = Math.PI * 0.5;
+  mesh.material = material;
+  scene.add(mesh);
 
-        // Update materials
-        updateAllMaterials()
-    }
-)
+  // Update materials
+  updateAllMaterials();
+});
 
 /**
  * Lights
  */
-const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.normalBias = 0.05
-directionalLight.position.set(0.25, 2, - 2.25)
-scene.add(directionalLight)
+const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.normalBias = 0.05;
+directionalLight.position.set(0.25, 2, -2.25);
+scene.add(directionalLight);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 1, - 4)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(4, 1, -4);
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
-})
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFShadowMap
-renderer.useLegacyLights = false
-renderer.toneMapping = THREE.ACESFilmicToneMapping
-renderer.toneMappingExposure = 1
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+  antialias: true,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
+renderer.useLegacyLights = false;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    // Update controls
-    controls.update()
+  // Update material
+  customUniforms.uTime.value = elapsedTime
 
-    // Render
-    renderer.render(scene, camera)
+  // Update controls
+  controls.update();
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Render
+  renderer.render(scene, camera);
 
-tick()
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
