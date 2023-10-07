0b325fa785acb4141b52119e5ea9749cef4a43df
Michela <mgaligigi@163.com>
2023-10-07 15:41:13 +0800

    refactor:删除Three-Plan

--- Projects/Three-Plan/src/Shaders/test/vertex.glsl
+++
@@ -1,9 +0,0 @@
-uniform mat4 projectionMatrix; //透视矩阵
-uniform mat4 viewMatrix; // 视图矩阵
-uniform mat4 modelMatrix; // 模型矩阵
-attribute vec3 position; // 传入的顶点坐标
-
-void main()
-{   
-  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1);
-}
