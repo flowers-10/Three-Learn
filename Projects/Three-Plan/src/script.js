60a34da821c31cb3fff3b75ac95b074b23055289
Michela <mgaligigi@163.com>
2023-10-07 14:16:17 +0800

    style(Three-Plan):添加注释

--- Projects/Three-Plan/src/script.js
+++ Projects/Three-Plan/src/script.js
@@ -8,29 +8,40 @@ import * as THREE from 'three'
 
 
 // Scene
+// Scene 场景就像一个容器。我们将对象、模型、粒子、灯光等放入其中，并在某个时候要求 Three.js 渲染该场景。
 
 
 /**
  * Object
  */
-
+// 形状 参数：长宽高
 // Geometry
 
+
 // Material
+// 材质 参数： 颜色
 
 // Mesh
+// 网格是几何体（形状）和材质的组合。
+
+/**
+ * Sizes
+ */
+
 
 /**
  * Camera
  */
 // Base camera
-
+// Camera 相机不可见。这更像是一种理论观点。当我们对场景进行渲染时，将从该摄像机的视觉角度进行渲染。(mvp会讲怎么做到的)
+// 参数一：视野
+// 参数二：纵横比
 
 
 /**
  * Renderer
  */
-
+// Renderer 渲染器的工作是进行渲染
 
 
 
