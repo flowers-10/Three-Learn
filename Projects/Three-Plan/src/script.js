8653d0e3b7a89cfa0af929b402f94b12841c8001
Michela <mgaligigi@163.com>
2023-10-07 14:25:37 +0800

    feat(Three-Plan):完整代码

--- Projects/Three-Plan/src/script.js
+++ Projects/Three-Plan/src/script.js
@@ -1,64 +1,93 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
 
 /**
  * Base
  */
+// Debug
+const gui = new dat.GUI({ width: 340 })
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-// Scene 场景就像一个容器。我们将对象、模型、粒子、灯光等放入其中，并在某个时候要求 Three.js 渲染该场景。
-const scene =  new THREE.Scene()
-
+const scene = new THREE.Scene()
 
 /**
  * Object
  */
-// 形状 参数：长宽高
 // Geometry
-const geometry = new THREE.BoxGeometry(1,1,1)
+const geometry = new THREE.PlaneGeometry(2, 2, 128, 128)
 
 // Material
-// 材质 参数： 颜色
-const material = new THREE.MeshBasicMaterial({color: 'red'})
+const material = new THREE.MeshBasicMaterial()
 
 // Mesh
-// 网格是几何体（形状）和材质的组合。
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
 /**
  * Sizes
  */
-const sizes =  {
-    width: 800,
-    height: 600,
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
 }
+
+window.addEventListener('resize', () =>
+  {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  })
 
 /**
  * Camera
  */
 // Base camera
-// Camera 相机不可见。这更像是一种理论观点。当我们对场景进行渲染时，将从该摄像机的视觉角度进行渲染。(mvp会讲怎么做到的)
-// 参数一：视野
-// 参数二：纵横比
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(1, 1, 1)
 scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
  */
-// Renderer 渲染器的工作是进行渲染
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
-
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene,camera)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
 
+const tick = () =>
+  {
+    const elapsedTime = clock.getElapsedTime()
 
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+  }
+
+tick()
