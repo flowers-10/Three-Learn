7d6b80909f97b72187c426fb2b0c5a02dc804c71
Michela <mgaligigi@163.com>
2023-10-26 17:58:27 +0800

    feat(33_Post-Processing):glitchPass 故障特效

--- code/33_Post-Processing/src/script.js
+++ code/33_Post-Processing/src/script.js
@@ -5,6 +5,8 @@ import * as dat from 'lil-gui'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
 import { DotScreenPass } from 'three/examples/jsm/postprocessing/DotScreenPass.js'
+import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass.js'
+
 
 /**
  * Base
@@ -144,6 +146,9 @@ effectComposer.addPass(renderPass)
 const dotScreenPass = new DotScreenPass()
 dotScreenPass.enabled = false
 effectComposer.addPass(dotScreenPass)
+const glitchPass = new GlitchPass()
+// glitchPass.goWild = true
+effectComposer.addPass(glitchPass)
 /**
  * Animate
  */
