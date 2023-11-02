ec5a5deb85b9dbe781e32df5b3f078dc78d6e8bc
Michela <mgaligigi@163.com>
2023-11-02 18:25:45 +0800

    feat(33_Post-Processing):Futuristic interface displacement

--- code/33_Post-Processing/src/script.js
+++ code/33_Post-Processing/src/script.js
@@ -247,6 +247,7 @@ const DisplacementShader = {
   uniforms: {
     tDiffuse: { value: null },
     uTime: { value: null },
+    uNormalMap: { value: null }
   },
   vertexShader: `
         varying vec2 vUv;
@@ -261,15 +262,19 @@ const DisplacementShader = {
   fragmentShader: `
         uniform sampler2D tDiffuse;
         uniform float uTime;
+        uniform sampler2D uNormalMap;
+
         varying vec2 vUv;
 
         void main()
         {   
-            vec2 newUv = vec2(
-                vUv.x,
-                vUv.y + sin(vUv.x * 10.0 + uTime) * 0.1
-            );
+            vec3 normalColor = texture2D(uNormalMap, vUv).xyz * 2.0 - 1.0;
+            vec2 newUv = vUv + normalColor.xy * 0.1;
             vec4 color = texture2D(tDiffuse, newUv);
+
+            vec3 lightDirection = normalize(vec3(- 1.0, 1.0, 0.0));
+            float lightness = clamp(dot(normalColor, lightDirection), 0.0, 1.0);
+            color.rgb += lightness * 2.0;
 
             gl_FragColor = color;
         }
@@ -278,6 +283,7 @@ const DisplacementShader = {
 
 const displacementPass = new ShaderPass(DisplacementShader);
 displacementPass.material.uniforms.uTime.value = 0;
+displacementPass.material.uniforms.uNormalMap.value = textureLoader.load('/textures/interfaceNormalMap.png')
 effectComposer.addPass(displacementPass);
 
 /**
