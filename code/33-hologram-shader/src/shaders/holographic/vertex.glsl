5d1d1c7828fc0973ca598ddd003f41df5f11be17
Michela <mgaligigi@163.com>
2024-05-03 22:58:09 +0800

    feat(33-hologram-shader):end

--- code/33-hologram-shader/src/shaders/holographic/vertex.glsl
+++ code/33-hologram-shader/src/shaders/holographic/vertex.glsl
@@ -1,9 +1,22 @@
 varying vec3 vPosition;
 varying vec3 vNormal;
+uniform float uTime;
+
+#include ../includes/random2D.glsl
+
 
 void main() {
     // Position
     vec4 modelPosition = modelMatrix * vec4(position , 1.);
+    // Glitch
+    float glitchTime = uTime - modelPosition.y;
+    float glitchStrength = sin(glitchTime) + sin(glitchTime * 3.45) +  sin(glitchTime * 8.76);
+    glitchStrength = smoothstep(0.3, 1.0, glitchStrength);
+    glitchStrength /= 3.0;
+    glitchStrength *= 0.25;
+
+    modelPosition.x += (random2D(modelPosition.xz + uTime) - 0.5) * glitchStrength;
+    modelPosition.z += (random2D(modelPosition.xz + uTime) - 0.5) * glitchStrength;
 
     // Final position
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
