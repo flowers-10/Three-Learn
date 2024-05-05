46f5e0635317694d3c3c1320c04ccc2e1c921f55
Michela <mgaligigi@163.com>
2024-05-05 13:56:01 +0800

    feat(59-load-models-with-r3f):end

---
+++ code/59-load-models-with-r3f/vite.config.js
@@ -0,0 +1,39 @@
+import react from '@vitejs/plugin-react'
+import { transformWithEsbuild } from 'vite'
+
+export default {
+    root: 'src/',
+    publicDir: '../public/',
+    base: './',
+    plugins:
+    [
+        // React support
+        react(),
+
+        // .js file support as if it was JSX
+        {
+            name: 'load+transform-js-files-as-jsx',
+            async transform(code, id)
+            {
+                if (!id.match(/src\/.*\.js$/))
+                    return null
+
+                return transformWithEsbuild(code, id, {
+                    loader: 'jsx',
+                    jsx: 'automatic',
+                });
+            },
+        },
+    ],
+    server:
+    {
+        host: true, // Open to local network and display URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: '../dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true // Add sourcemap
+    },
+}
