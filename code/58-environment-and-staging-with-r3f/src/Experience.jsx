49ca3d05868bcae76f9296e1148532d4bc1e33b2
Michela <mgaligigi@163.com>
2024-05-05 11:12:13 +0800

    feat(58-environment-and-staging-with-r3f):shadow end

--- code/58-environment-and-staging-with-r3f/src/Experience.jsx
+++ code/58-environment-and-staging-with-r3f/src/Experience.jsx
@@ -1,40 +1,96 @@
-import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { useRef } from 'react'
-import { Perf } from 'r3f-perf'
+import { useFrame } from "@react-three/fiber";
+import {
+  RandomizedLight,
+  useHelper,
+  OrbitControls,
+  AccumulativeShadows,
+  BakeShadows,
+  SoftShadows,
+  ContactShadows,
+} from "@react-three/drei";
+import { useRef } from "react";
+import { Perf } from "r3f-perf";
+import * as THREE from "three";
+import { useControls } from "leva";
 
-export default function Experience()
-{
-    const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
-        cube.current.rotation.y += delta * 0.2
+export default function Experience() {
+    const { color, opacity, blur } = useControls('contact shadows', {
+        color: '#1d8f75',
+        opacity: { value: 0.4, min: 0, max: 1 },
+        blur: { value: 2.8, min: 0, max: 10 },
     })
 
-    return <>
+  const cube = useRef();
+  const directionalLight = useRef();
+  useHelper(directionalLight, THREE.DirectionalLightHelper, 1);
 
-        <Perf position="top-left" />
+  useFrame((state, delta) => {
+    cube.current.rotation.y += delta * 0.2;
+    // const time = state.clock.elapsedTime;
+    // cube.current.position.x = 2 + Math.sin(time);
+  });
 
-        <OrbitControls makeDefault />
-
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
-
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+  return (
+    <>
+      <ContactShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        resolution={512}
+        far={5}
+        color={color}
+        opacity={opacity}
+        blur={blur}
+        frames={ 1 }
+      />
+      {/* <SoftShadows size={25} samples={10} focus={0}></SoftShadows> */}
+      {/* <BakeShadows /> */}
+      {/* <AccumulativeShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        color="#316d39"
+        opacity={0.8}
+        frames={Infinity}
+        temporal
+        blend={ 100 }
+      >
+        <RandomizedLight
+          amount={8}
+          radius={1}
+          ambient={0.5}
+          intensity={3}
+          position={[1, 2, 3]}
+          bias={0.001}
+        />
+      </AccumulativeShadows> */}
+      <color args={["ivory"]} attach="background" />
+      <Perf position="top-left" />
+      <OrbitControls makeDefault />
+      <directionalLight
+        ref={directionalLight}
+        castShadow
+        position={[1, 2, 3]}
+        intensity={4.5}
+        shadow-mapSize={[1024, 1024]}
+        shadow-camera-near={1}
+        shadow-camera-far={10}
+        shadow-camera-top={5}
+        shadow-camera-right={5}
+        shadow-camera-bottom={-5}
+        shadow-camera-left={-5}
+      />
+      <ambientLight intensity={1.5} />
+      <mesh castShadow position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+      <mesh castShadow ref={cube} position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
     </>
+  );
 }
--- code/58-environment-and-staging-with-r3f/src/index.jsx
+++ code/58-environment-and-staging-with-r3f/src/index.jsx
@@ -1,19 +1,21 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Experience from './Experience.jsx'
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-    <Canvas
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ - 4, 3, 6 ]
-        } }
-    >
-        <Experience />
-    </Canvas>
-)
+  <Canvas
+  shadows={false}
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 200,
+      position: [-4, 3, 6],
+    }}
+  >
+    {/* <color args={ [ '#ff0000' ] } attach="background" /> */}
+    <Experience />
+  </Canvas>
+);
