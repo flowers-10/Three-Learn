5b73c3d54acbbfc7c8685b83d8f9347670403a85
Michela <mgaligigi@163.com>
2024-05-26 22:40:16 +0800

    feat(63-post-processing-with-r3f):starter pack

---
+++ code/63-post-processing-with-r3f/vite.config.js
@@ -0,0 +1,42 @@
+import react from '@vitejs/plugin-react'
+import { transformWithEsbuild } from 'vite'
+import restart from 'vite-plugin-restart'
+
+export default {
+    root: 'src/',
+    publicDir: '../public/',
+    plugins:
+    [
+        // Restart server on static/public file change
+        restart({ restart: [ '../public/**', ] }),
+
+        // React support
+        react(),
+
+        // .js file support as if it was JSX
+        {
+            name: 'load+transform-js-files-as-jsx',
+            async transform(code, id)
+            {
+                if (!id.match(/src\/.*\.js$/))
+                    return null
+
+                return transformWithEsbuild(code, id, {
+                    loader: 'jsx',
+                    jsx: 'automatic',
+                });
+            },
+        },
+    ],
+    server:
+    {
+        host: true, // Open to local network and display URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: '../dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true // Add sourcemap
+    },
+}
